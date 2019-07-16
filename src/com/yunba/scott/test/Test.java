4b27f9c2ad9776e695d3a7f2c42950405e2f8635
yunbaScott <643650655@qq.com>
2019-07-16 14:07:30 +0800

    新增输出功能-develop

--- src/com/yunba/scott/test/Test.java
+++ src/com/yunba/scott/test/Test.java
@@ -3,5 +3,7 @@ package com.yunba.scott.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		
+		System.out.println("这个是在develop这个版本上面增加开发的功能");
 	}
 }
