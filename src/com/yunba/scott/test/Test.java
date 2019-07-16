ab677f08941bef340189e1fbb457c12b418190a1
yunbaScott <643650655@qq.com>
2019-07-16 14:15:12 +0800

    新开发功能-rexlase

--- src/com/yunba/scott/test/Test.java
+++ src/com/yunba/scott/test/Test.java
@@ -3,5 +3,6 @@ package com.yunba.scott.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		System.out.println("这个是新开发的功能-rexlase");
 	}
 }
